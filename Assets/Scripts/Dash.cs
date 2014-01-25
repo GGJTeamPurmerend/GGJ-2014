97bd2c2cc999db94c1569d93f8ce074b45514fc1
BaZaH <bas.philippus@gmail.com>
2014-01-25 04:46:21 +0100

    Dash start + controller support

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dash : MonoBehaviour 
+{
+    public int dashDistance;
+
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            
+        }
+	}
+
+    void dash(Vector3 direction)
+    {
+        //rigidbody.AddForce(
+    }
+}
--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -4,25 +4,35 @@ using System.Collections;
 [System.Serializable]
 public class Boundary
 {
-	public float xMin, xMax, zMin, zMax;
+    public float xMin, xMax, zMin, zMax;
 }
 
-public class Controller : MonoBehaviour {
+public class Controller : MonoBehaviour
+{
 
-	public float speed;
-	public Boundary boundary;
+    public float speed;
+    public Boundary boundary;
 
-	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rigidbody.velocity = movement * speed;
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (movement.magnitude > 0.25)
+        {
+            rigidbody.velocity = movement * speed;
+            this.transform.rotation = Quaternion.LookRotation(movement);
+        }
+        else
+        {
+            rigidbody.velocity = Vector3.zero;
+        }
 
-		rigidbody.position = new Vector3(
-			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
-			0.0f,
-			Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
-		);
-	}
+        rigidbody.position = new Vector3(
+            Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax)
+        );
+    }
 }
