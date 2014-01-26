5d64ce7839f080fd2ca916f886014d7491a94512
MitchellEnsink <mitchellensink@live.nl>
2014-01-26 01:40:31 +0100

    Added repeating background.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+	private float offsetX;
+
+	// Use this for initialization
+	void Start() {
+		renderer.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate() {
+		renderer.material.mainTextureOffset = new Vector2(Camera.main.transform.position.x / 25, Camera.main.transform.position.z / 25);
+	}
+}
