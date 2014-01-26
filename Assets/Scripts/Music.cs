d9560015fa517c91660af006484201310aec953a
MitchellEnsink <mitchellensink@live.nl>
2014-01-26 03:30:02 +0100

    Added MusicManager for persistent music,

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+	private static Music instance = null;
+
+	// Use this for initialization
+	void Awake() {
+		if(instance != null) {
+			Destroy(this.gameObject);
+		}
+		else {
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+	}
+}
